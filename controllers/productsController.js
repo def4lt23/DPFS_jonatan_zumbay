504565bc5eb9f8e4b7b9ec77f697fcc8fd5c0e71
¨jonatan_z¨ <jonezmby@gmail.com>
2025-08-07 23:39:10 -0300

    agregados productos de forma dinamica

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,6 +1,13 @@
+const fs = require('fs') //file system
+const path = require('path') // manejar las rutas
+
+const productsPath = path.join(__dirname, '../data/products.json'); // ruta al archivo de productos
+
 const productsController = {
   productos: function (req, res, next) {
-    res.render("products/productos", { title: "Productos" });
+    const productosjs = JSON.parse(fs.readFileSync(productsPath, 'utf-8')) // leer el archivo y convertirlo a un objeto js
+
+    res.render("products/productos", {productosjs});
   }
 
   //si quiero agregar coloco una coma y abajo el siguiente metodo.
